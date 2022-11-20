0735bbf53036c0b1ae4b883c9f9789cb0da9e250
KseniiaS <myNew@email.com>
2022-11-20 08:53:20 +0300

    Добавлена папка Exmples002 и .gitignore

---
+++ Exemples002/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write ("Введите ваш логин");
+string username = Console.ReadLine();
+Console.Write ("Привет,");
+Console.Write (username);
