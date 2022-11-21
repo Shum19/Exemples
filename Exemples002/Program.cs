f60ee0033aa7574a2e922824e3567bbdc6199bb8
KseniiaS <myNew@email.com>
2022-11-21 07:08:01 +0300

    Удалил лишнею запист git во 2-й строчке

--- Exemples002/Program.cs
+++ Exemples002/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write git("Введите ваш логин");
+Console.Write ("Введите ваш логин");
 string username = Console.ReadLine();
 Console.Write ("Привет,");
 Console.Write (username);
