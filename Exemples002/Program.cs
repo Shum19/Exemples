8717ae8cc65fd760ba6efe88541b429cda47decb
KseniiaS <myNew@email.com>
2022-11-21 06:29:23 +0300

    Добавлены папки E[emples002&003_Sum

--- Exemples002/Program.cs
+++ Exemples002/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write ("Введите ваш логин");
+Console.Write git("Введите ваш логин");
 string username = Console.ReadLine();
 Console.Write ("Привет,");
 Console.Write (username);
