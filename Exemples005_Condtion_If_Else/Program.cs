6aa69955d13262b51bc3acf57115a6797e509cdb
KseniiaS <myNew@email.com>
2022-11-21 07:35:56 +0300

    Добавлена папак Exemples 005 Condition If Else

---
+++ Exemples005_Condtion_If_Else/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введдите ваше имя ");
+string username = Console.ReadLine();
+ 
+if (username.ToLower() == "маша") {
+    Console.WriteLine ("Ура это же МАША!");
+}
+else
+{
+    Console.WriteLine("Привет,");
+}
